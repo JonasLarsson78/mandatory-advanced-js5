2fabef4bbb378192b93ebc79b17b7892073a4067
Andreas Iwerbo <andreas@iwerbo.se>
2019-04-25 10:33:11 +0200

    added navigate function to list component just to test

--- src/Components/listitems.js
+++ src/Components/listitems.js
@@ -1,18 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {token$} from './store.js';
+import { Dropbox } from 'dropbox';
 
 const ListItems = (props) => {
-  console.log(props.listData)
-  const data = props.listData
+
+  const [data, updateData] = useState(props.listData)
+
+
+  const navigate = (e) => {
+
+   console.log(e.target.dataset.folder)
+    //updateFolder(e.target.dataset.folder)
+
+    const option = {
+      fetch: fetch,
+      accessToken: token$.value
+    };
+    
+    const dbx = new Dropbox(
+      option,
+    );
+    dbx.filesListFolder({
+      path: e.target.dataset.folder
+    })
+    .then(response => {
+      console.log(response)
+      updateData(response.entries)
+      
+      
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
+
+  }
 
   const renderList = (data) => {
     return(
-      <li key={data.id}>{data.name}</li>
+      <li onClick={navigate} key={data.id} data-folder={data.path_lower}>{data.name}</li>
     )
   }
 
   const listData = data.map(renderList)
-
- 
   
 
   return(
