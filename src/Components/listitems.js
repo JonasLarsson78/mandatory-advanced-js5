340163d9f812fb1d54545eb60f7d505898233fea
Andreas Iwerbo <andreas@iwerbo.se>
2019-04-28 16:24:11 +0200

    added last edited function

--- src/Components/listitems.js
+++ src/Components/listitems.js
@@ -4,6 +4,8 @@ import { Dropbox } from 'dropbox';
 import { BrowserRouter as Router, Route, Link, Redirect}from "react-router-dom";
 import '../Css/listitems.css';
 import { downloadFile } from './dowload'
+import moment from 'moment';
+
 
 
 
@@ -106,10 +108,23 @@ let newFolder = props.folder;
 
 
   const readableBytes = (bytes) => {
-    const i = Math.floor(Math.log(bytes) / Math.log(1024)),
+    const index = Math.floor(Math.log(bytes) / Math.log(1024)),
     sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   
-    return (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, index)).toFixed(2) * 1 + ' ' + sizes[index];
+  }
+
+  const lastEdited = (str) => {
+    console.log(str)
+    const year = str.substring(0, 4);
+    const month = str.substring(5, 7)
+    const day = str.substring(8, 10)
+    const hour = str.substring(11, 13)
+    const minute = str.substring(14, 16)
+    const second = str.substring(17, 19)
+
+    return <label>{'Last edited: ' + moment(str).fromNow() + ', ' + year + '-' + month + '-' + day + '.'}</label>
+    
   }
 
  
@@ -119,7 +134,7 @@ let newFolder = props.folder;
     if(data[".tag"] === 'file'){
 
       return(
-        <li title={"Download: " + data.name} key={data.id} className="listFiles" to={data.path_lower} data-name={data.name} onClick={downloadFile} data-folder={data.path_lower} data-tag={data[".tag"]}>{data.name}{readableBytes(data.size)}</li>
+        <li title={"Download: " + data.name} key={data.id} className="listFiles" to={data.path_lower} data-name={data.name} onClick={downloadFile} data-folder={data.path_lower} data-tag={data[".tag"]}>{data.name}<label> {readableBytes(data.size)}{lastEdited(data.server_modified)}</label></li>
       )
     }
       return(
