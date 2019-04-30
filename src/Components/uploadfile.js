676f1fea697dfacccdafd796a07c60c6388f63b6
Andreas Iwerbo <andreas@iwerbo.se>
2019-04-30 15:17:34 +0200

    upload feature done

--- src/Components/uploadfile.js
+++ src/Components/uploadfile.js
@@ -9,13 +9,11 @@ const UploadFile = () => {
     e.preventDefault();
     
     const fileNodeList = inputRef.current.files;
-    const fileList = Array.from(fileNodeList)
-
-    
     const filesiZeLimit = 150 * 1024 * 1024;
 
-    
 
+    const fileList = Array.from(fileNodeList)
+  
     const option = {
       fetch: fetch,
       accessToken: token$.value,
@@ -26,31 +24,83 @@ const UploadFile = () => {
     );
     
    
+    for(let i = 0; i < fileList.length; i++){
+
+      if(fileList[i].size < filesiZeLimit){
+
+        dbx.filesUpload({  
+        
+          contents: fileList[i],
+          path: '/' + fileList[i].name
+          })
+
+              .then(function(response) {
+                
+             
+                console.log(response);
+              })
+              .catch(function(error) {
+                console.error(error);
+              });
+
+
+      }
+      else{
+        const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
+        var workItems = [];     
+      
+        var offset = 0;
+        while (offset < fileList[i].size) {
+          var chunkSize = Math.min(maxBlob, fileList[i].size - offset);
+          workItems.push(fileList[i].slice(offset, offset + chunkSize));
+          offset += chunkSize;
+        } 
+          
+        const task = workItems.reduce((acc, blob, idx, items) => {
+          if (idx === 0) {
+            console.log(idx)
+            // Starting multipart upload of file
+            return acc.then(function() {
+              console.log('start')
+              return dbx.filesUploadSessionStart({ close: false, contents: blob})
+                        .then(response => response.session_id)
+            });          
+          } else if (idx < items.length-1) { 
+            console.log('append') 
+            // Append part to the upload session
+            return acc.then(function(sessionId) {
+             var cursor = { session_id: sessionId, offset: idx * maxBlob };
+             return dbx.filesUploadSessionAppendV2({ cursor: cursor, close: false, contents: blob }).then(() => sessionId); 
+            });
+          } else {
+            // Last chunk of data, close session
+            return acc.then(function(sessionId) {
+              var cursor = { session_id: sessionId, offset: fileList[i].size - blob.size };
+              var commit = { path: '/' + fileList[i].name, mode: 'add', autorename: true, mute: false };              
+              return dbx.filesUploadSessionFinish({ cursor: cursor, commit: commit, contents: blob });           
+            });
+          }          
+        }, Promise.resolve());
+        
+        task.then(function(result) {
+          
+          console.log(result)
+        }).catch(function(error) {
+          console.error(error);
+        });
+        
+      }
+        
+
+       
+    
+       console.log(offset)
+       console.log(workItems)
+
+        
+      
       
 
-     
-
-    
-    for(let i = 0; i < fileList.length; i++){
-      console.log(fileList[i])
-
-      dbx.filesUpload({  
-        
-       
-       contents: fileList[i],
-       path: '/' + fileList[i].name
-       })
- 
-     
- 
-           .then(function(response) {
-             
-          
-             console.log(response);
-           })
-           .catch(function(error) {
-             console.error(error);
-           });
 
     } 
     
