25038a19b2a9dcdf8d27d9338a2f20841b211871
Jim Nervall <nervall@hotmail.com>
2019-05-02 13:31:16 +0200

    breacrumb home css

--- src/Components/breadcrumbs.js
+++ src/Components/breadcrumbs.js
@@ -5,11 +5,13 @@ import '../Css/breadcrumbs.css';
 
 const Breadcrumbs = withBreadcrumbs([{ path: '/', breadcrumb: null }])(({ breadcrumbs }) => (
     <>
+    <i class="material-icons-outlined breadcrumb-home">home</i>
     <React.Fragment>
       {breadcrumbs.map(({
         match,
         breadcrumb,
       }) => (
+        
         <span key={match.url}>
           <NavLink to={match.url} className="breadcrumb-link"> {breadcrumb} 
           </NavLink><i className="material-icons breadcrumb">arrow_right</i>
