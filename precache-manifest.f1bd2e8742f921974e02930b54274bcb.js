18f49dc0187d2c7f860f24983f21f8e80a3eed1b
JonasLarsson78 <jl.7804@gmail.com>
2019-05-16 00:44:58 +0200

    Updates

---
+++ precache-manifest.f1bd2e8742f921974e02930b54274bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef546021fd325737a8b85e212dfdee2",
+    "url": "/mandatory-advanced-js5/index.html"
+  },
+  {
+    "revision": "37c94cc668787a3f75c0",
+    "url": "/mandatory-advanced-js5/static/css/main.cac5c930.chunk.css"
+  },
+  {
+    "revision": "f732d4e36ba764989484",
+    "url": "/mandatory-advanced-js5/static/js/2.83bf2fb4.chunk.js"
+  },
+  {
+    "revision": "37c94cc668787a3f75c0",
+    "url": "/mandatory-advanced-js5/static/js/main.a151e6ae.chunk.js"
+  },
+  {
+    "revision": "693ed353e3382e7179ea",
+    "url": "/mandatory-advanced-js5/static/js/runtime~main.d0f14806.js"
+  },
+  {
+    "revision": "91350eb77a1c8271b036bf1a6796b8c6",
+    "url": "/mandatory-advanced-js5/static/media/Logo_mybox.91350eb7.png"
+  }
+]);
