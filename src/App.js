e593524588b92ddd969d3ecd4a7e2ae7cb673b84
JonasLarsson78 <jl.7804@gmail.com>
2019-05-16 01:46:59 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './Components/login';
 import Auth from './Components/auth';
 import Home from './Components/home';
-import { BrowserRouter as Router, Route}from "react-router-dom";
+import { ReactRouter as Router, Route}from "react-router-dom";
 
 
 
