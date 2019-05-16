8754ccd0754af571ba5992b0a528ede4fc255a6f
JonasLarsson78 <jl.7804@gmail.com>
2019-05-16 08:29:03 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './Components/login';
 import Auth from './Components/auth';
 import Home from './Components/home';
-import { BrowserRouter as Router, Route}from "react-router-dom";
+import { HachRouter as Router, Route}from "react-router-dom";
 
 
 
@@ -11,9 +11,16 @@ function App() {
 
   return (
     <Router>
+    <div className="App">
+    
+    
+    </div>
+    
     <Route exact path='/' component={Login} />
     <Route path='/auth' component={Auth} />
     <Route path='/home' component={Home} />
+
+   
     </Router>
   );
 }
